ef4df827e419a4bc51067f14d725042364fc612c
jesion <7040210@qq.com>
2017-09-24 18:25:44 +0800

    修改配置文件信息

--- super-boot-user-center/src/main/java/org/superboot/StartUserCenter.java
+++ super-boot-user-center/src/main/java/org/superboot/StartUserCenter.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -26,11 +27,9 @@ import javax.annotation.PostConstruct;
  * @Path org.superboot.StartUserCenter
  */
 @EnableSwagger2Doc
-@SpringBootApplication
 @EnableCaching
-@EnableDiscoveryClient
+@SpringCloudApplication
 @EnableFeignClients
-@EnableCircuitBreaker
 @RefreshScope
 public class StartUserCenter {
 
